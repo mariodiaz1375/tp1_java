c5d79ba0971a8c62f3b622309001114876e214fb
Mario <mariodiaz1375@gmail.com>
2025-06-01 19:50:47 -0300

    ejercicio1 terminado

---
+++ 1/index.js
@@ -0,0 +1,17 @@
+let numero = parseInt(prompt("Ingrese un numero"))
+console.log("boca bocaaa")
+if (numero > 0) {
+    alert("El numero es positivo")
+    console.log(numero)
+}
+else if (numero < 0) {
+    alert("El numero es negativo")
+    console.log(numero)
+}
+else if (numero == 0) {
+    alert("El numero es cero")
+    console.log(numero)
+}
+else {
+    alert("Ingrese solo numeros")
+}
