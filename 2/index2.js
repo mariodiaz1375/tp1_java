b5cfd25f9ce2c01dd82b608a7737b933a47b876e
mario <mariodiaz1375@gmail.com>
2025-06-01 20:18:16 -0300

    se termino el ejercicio2

--- 2/index2.js
+++ 2/index2.js
@@ -1,5 +1,11 @@
 let contar = parseInt(prompt('Ingrese un numero'))
-for (i=numero; i=1; i--) {
-    let numero = numero -1
-    console.log('El numero es: ', numero)
+console.log('probando')
+if (contar > 0) {
+    for (let i = contar; i>=1; i--) {
+        console.log('El numero es: ', i)
+    }
+    console.log('DESPEGUE!!')
+}
+else {
+    console.log('Ingresa un numero positivo para la cuenta regresiva')
 }
